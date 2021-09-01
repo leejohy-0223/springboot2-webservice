6d4db03c61f4ec3b2bdf06489e547493d03e5957
leejohy <leejohy@naver.com>
2021-09-01 20:12:21 +0900

    fixed untracked file

--- src/main/java/com/leejohy/book/springboot/web/PostApiController.java
+++ src/main/java/com/leejohy/book/springboot/web/PostApiController.java
@@ -14,17 +14,23 @@ public class PostApiController {
     private final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) { // id 반환
         return postsService.save(requestDto);
     }
 
     @PutMapping("/api/v1/posts/{id}")
-    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) { // id 반환
         return postsService.update(id, requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id}")
+    @GetMapping("/api/v1/posts/{id}") // dto를 반환해서, 브라우저로 요청하면 json 반환
     public PostsResponseDto findById (@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 }
